ac97967d5141973ba9dffc5e0b01256e7c79d5cb
Øystein Sigholt <oysteinsigholt@users.noreply.github.com>
2018-04-30 16:38:59 -0700

    test

--- app.js
+++ app.js
@@ -17,51 +17,33 @@ function hex(buffer) {
 
   return hexCodes.join("");
 }
+$(function() {
+  console.log("loaded");
+  $(".video").each(function() {
+    var video = $( this ).find( "video" )[0];
+    var hash = $( this ).find( "hash" )[0];
 
-function getVideoImage(path, secs, callback) {
-  var me = this, video = document.createElement('video');
-  console.log(video);
-  video.onloadedmetadata = function() {
-    console.log("onloadedmetadata");
-    if ('function' === typeof secs) {
-      secs = secs(this.duration);
-    }
-    this.currentTime = Math.min(Math.max(0, (secs < 0 ? this.duration : 0) + secs), this.duration);
-  };
-  video.onseeked = function(e) {
-    console.log("seek");
-    var canvas = document.createElement('canvas');
-    document.body.append(canvas);
-    canvas.height = video.videoHeight;
-    canvas.width = video.videoWidth;
-    var ctx = canvas.getContext('2d');
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    video.onseeked = function(e) {
+      var canvas = document.createElement('canvas');
+      document.body.append(canvas);
+      canvas.height = video.videoHeight;
+      canvas.width = video.videoWidth;
+      var ctx = canvas.getContext('2d');
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-    try {
-      var data = ctx.getImageData(0, 0, canvas.width, canvas.height);
-      sha256(data).then(function(digest) {
-        callback(digest);
-      }, function(error) {
-        console.log(error);
-        callback(null);
-      });
-    } catch(e) {
-      console.log(e);
-      callback(null);
-    }
-  };
-  video.onerror = function(e) {
-    console.log("err");
-    callback(null);
-  };
-  video.src = path;
-  document.body.append(video);
-  console.log("Appended");
-}
-
-getVideoImage("video.mp4", 0, function(digest) {
-  console.log("Digested");
-	document.body.append(digest);
+      try {
+        var data = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        sha256(data).then(function(digest) {
+          hash.innerHTML = digest;
+        }, function(error) {
+          console.log(error);
+          hash.innerHTML = null;
+        });
+      } catch(e) {
+        console.log(e);
+        hash.innerHTML = null;
+      }
+    };
+    video.currentTime = 0;
+  });
 });
-
-console.log("Loaded");
