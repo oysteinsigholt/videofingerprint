369ac69231af38bcb0b6bb1e465eca04f73bf103
Øystein Sigholt <oysteinsigholt@users.noreply.github.com>
2018-05-06 19:53:39 -0700

    speed up

--- video/generate.py
+++ video/generate.py
@@ -5,7 +5,7 @@ command =  "ffmpeg -loop 1 -i PM5644.png -c:v libx264 -t 1 -pix_fmt yuv420p -vf 
 profiles = ["baseline", "main", "high", "high10", "high422", "high444"]
 
 for profile in profiles:
-	level = 6
-	for sublevel in range(0,3):
-		process = subprocess.Popen(command.format(profile,level,sublevel), shell=True, stdout=subprocess.PIPE)
-		process.wait()
+	for level in [0, 1, 3, 6]:
+		for sublevel in [0, 2, 9]:
+			process = subprocess.Popen(command.format(profile,level,sublevel), shell=True, stdout=subprocess.PIPE)
+			process.wait()
